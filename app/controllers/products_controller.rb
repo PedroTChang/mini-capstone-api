ddd8cfb2ba9cbcce6f649525da857ae3ee7bf566
Pedro Chang <pedrochang91@gmail.com>
2022-05-04 09:44:15 -0700

    Creates product_all action in products controller, and adds to routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,6 @@
 class ProductsController < ApplicationController
+  def product_all
+    products = Product.all
+    render json: product.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "product_all" => "products#product_all"
 end
