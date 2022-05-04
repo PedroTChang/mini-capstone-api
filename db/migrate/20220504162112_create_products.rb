1d6ac49e38f85a6125eeb8faf55498bb5a5e7cf4
Pedro Chang <pedrochang91@gmail.com>
2022-05-04 09:22:55 -0700

    Pre-migrate version of Product model

---
+++ db/migrate/20220504162112_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.integer :price
+      t.string :image_url
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
