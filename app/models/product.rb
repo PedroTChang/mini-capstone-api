79080b1d68784f8712b29bd4957907956a3dc545
Pedro Chang <pedrochang91@gmail.com>
2022-05-19 10:32:11 -0700

    Implements relationship between carted_products & product

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,6 +11,7 @@ class Product < ApplicationRecord
   has_many :orders
   has_many :category_products
   has_many :categories, through: :category_products
+  has_many :carted_products
 
   def dollar_price
     return "$#{price}"
