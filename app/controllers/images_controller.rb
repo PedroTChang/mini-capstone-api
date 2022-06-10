357bd2dda9b0818c7729be1c825ccf9f7387eff1
Pedro Chang <pedrochang91@gmail.com>
2022-06-10 13:13:22 -0700

    adds validations

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,15 +1,15 @@
 class ImagesController < ApplicationController
-  # def index
-  #   @images = Images.where(product_id :id)
-  #   render template: images/index
-  # end
+  def index
+    @images = Images.where(product_id :id)
+    render template: images/index
+  end
 
-  # def show
-  #   @image = Image.find_by(product_id :id)
-  #   render json:
-  # end
+  def show
+    @image = Image.find_by(product_id :id)
+    render json:
+  end
 
-  # def create
-  #   @image = Image
-  # end
+  def create
+    @image = Image
+  end
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3000", "mini-capstone.peterxjang.com"
+    origins "example.com", "localhost:8080"
 
     resource "*",
       headers: :any,
